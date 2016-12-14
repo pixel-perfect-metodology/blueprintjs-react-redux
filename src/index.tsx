c528dcbc3d95e0e67c8f6695b6694e9c6f818d1f
Kunjan <kunjee17@gmail.com>
2016-12-14 09:12:18 +0530

    project working

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import {App} from "./containers/App";
+import { App } from "./containers/App";
 
 const rootEl = document.getElementById("root");
 ReactDOM.render(
-    <AppContainer>
-        <App />
-    </AppContainer>,
-    rootEl
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
 );
 
 // Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept("./containers/App", () => {
+if ((module as any).hot) {
+  (module as any).hot.accept("./containers/App", () => {
     const NextApp = require("./containers/App").default;
     ReactDOM.render(
       <AppContainer>
-        <NextApp/>
+        <NextApp />
       </AppContainer>
       ,
       rootEl
     );
   });
-} 
+}
